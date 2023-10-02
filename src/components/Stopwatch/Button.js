3de9d01b6ae3db2bef2b007b0d8866dfff30be68
Kirankumar866 <kirankumarparasa9996@gmail.com>
2023-10-02 20:48:35 +0000

    commit message

---
+++ src/components/Stopwatch/Button.js
@@ -0,0 +1,9 @@
+const Button = () => (
+  <li>
+    <button type="button">Start</button>
+    <button type="button">Stop</button>
+    <button type="button">Reset</button>
+  </li>
+)
+
+export default Button
---
+++ src/components/Stopwatch/index.js
@@ -0,0 +1,48 @@
+// Write your code here
+import {Component} from 'react'
+import './index.css'
+
+const initailvalues = {
+  initialMinutes: 0,
+  initialSeconds: 0,
+}
+
+class Stopwatch extends Component {
+  state = initailvalues
+
+  increment = () => {
+    this.setState(prev => {
+      const {initialSeconds} = prev
+      return {initialSeconds: initialSeconds + 1}
+    })
+  }
+
+  onStartbtn = () => {
+    const timerId = setInterval(() => this.increment, 1000)
+  }
+
+  render() {
+    const {initialMinutes, initialSeconds} = this.state
+    return (
+      <div className="bigCon">
+        <h1>StopWatch</h1>
+        <div className="timmerCon">
+          <h1>Timer</h1>
+          <div>
+            <p>{initialSeconds}</p>
+            <p>:</p>
+            <p>{initialMinutes}</p>
+          </div>
+          <div>
+            <button type="button" onClick={this.onStartbtn}>
+              Start
+            </button>
+            <button type="button">Stop</button>
+            <button type="button">Reset</button>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+export default Stopwatch
